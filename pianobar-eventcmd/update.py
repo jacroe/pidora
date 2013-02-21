86c615264a44227c8963b0dd17a11fd1224194a9
Jacob Roeland <jacob@jacroe.com>
2013-02-20 18:22:32 -0600

    Forgot to include update.py... whoops

--- pianobar-eventcmd/update.py
+++ pianobar-eventcmd/update.py
@@ -24,3 +24,10 @@ elif event == "songban":
 	open(www + "msg", "w").write("Banned")
 elif event == "songshelf":
 	open(www + "msg", "w").write("Tired")
+elif event == "usergetstations":				# Code thanks to @officerNordBerg on GitHub
+        stationCount = int(fields["stationCount"])
+	stations = ""
+	for i in range(0, stationCount):
+		stations += "%s="%i + fields["station%s"%i] + "|" 
+	stations = stations[0:len(stations) - 1]
+	open(www + "stationList", "w").write(stations)
