c720dcf027047563c760926ed8259a0e60eca569
Jacob Roeland <jacob@jacroe.com>
2013-07-31 22:37:27 -0500

    Updated install.sh script  - This should work out of the box.  - However, it hasn't been tested yet on a vanilla raspberrypi (sounds delicious).  - Implements #25

--- install.sh
+++ install.sh
@@ -1,33 +1,9 @@
 cd $HOME
 echo "Installing packages. This may take a while..."
-sudo apt-get install apache2 libapache2-mod-php5 pianobar git mpg123 python-feedparser -y
+sudo apt-get install pianobar git mpg123 python-feedparser -y
 echo "All packages installed"
-echo "Restarting Apache just for kicks"
-sudo /etc/init.d/apache2 restart
-mkdir ~/www
-sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/pidora
-echo "We're about to open a config file with nano. Please change each instance of '/var/www/' to '$HOME/www/'"
-read -n1 -r -p "Press any key to continue..."
-sudo nano /etc/apache2/sites-available/pidora
-echo "Thanks. Enabling the new profile..."
-sudo a2dissite default && sudo a2ensite pidora
-echo "...and restarting Apache again"
-sudo /etc/init.d/apache2 restart
-echo "Testing Apache configuration"
-echo "pidora" > /home/pi/www/index.html
-if [[ $(wget -qO- http://localhost/) ]]
-   then echo "Apache is up and running!"
-else (
-   echo "Ugh... Could not setup Apache"
-   echo "Exiting..."
-   return
-)
-fi
-echo "Making sure PHP is enabled"
-sudo a2enmod php5
-sudo /etc/init.d/apache2 restart
-echo "Finished installing Apache and PHP"
-echo
+sleep 5
+clear
 echo "Starting to set up pianobar"
 read -p "What is your Pandora email address? " username
 read -p "What is your Pandora pasword? " password
@@ -35,29 +11,48 @@ mkdir -p ~/.config/pianobar
 echo "user = $username
 password = $password" > ~/.config/pianobar/config
 fingerprint=`openssl s_client -connect tuner.pandora.com:443 < /dev/null 2> /dev/null | openssl x509 -noout -fingerprint | tr -d ':' | cut -d'=' -f2` && echo tls_fingerprint = $fingerprint >> ~/.config/pianobar/config
+echo
 echo "We think we set up pianobar. We'll test it now. It should log in and ask you to select a station."
+echo "After selecting the station, pianobar will print the station's name and a long ID number. Copy that number to the clipboard."
+echo "Press q to quit at any time."
+read -n1 -r -p "Press any key to continue..."
+echo
 pianobar
-echo "If it didn't work, you'll need to troubleshoot it yourself."
+echo "If for whatever reason it didn't work, you'll need to troubleshoot it yourself."
 echo "Once you've done so, we'll start setting up the rest of it."
 read -n1 -r -p "Press any key to continue..."
 echo
-rm -rf ~/www/
-echo "We'll now start cloning into Pidora"
+read -p "What was the station's ID number? " stationID
+echo "autostart_station = $stationID" >> ~/.config/pianobar/config
+cd $HOME
+clear
+echo "Thanks. We'll now start cloning into Pidora"
 git clone https://github.com/jacroe/pidora.git -q
-echo "Downloading finished. Setting up now"
-mv pidora/* www/
-sudo chgrp www-data www/
-chmod g+w www/
-mkdir www/albumart/
-sudo chgrp www-data www/albumart/
-chmod g+w www/albumart/
+echo "Downloading finished."
+echo
+echo "We'll begin setting up Pidora for use."
 echo "Creating FIFO queue"
-mkfifo www/ctl
-chmod g+w www/ctl
-sudo chgrp www-data www/ctl
+mkfifo pidora/ctl
 echo "Adding new variables to pianobar's config"
-echo "event_command = ~/www/pianobar-eventcmd/update.py
-fifo = ~/www/ctl" >> ~/.config/pianobar/config
-echo "We're going to open a python script with nano. Please set the variable 'www' to '$HOME/www/'"
+echo "event_command = $HOME/pidora/bar-update.py
+fifo = $HOME/pidora/ctl" >> ~/.config/pianobar/config
+echo "We think we're done. Let's test it, shall we?"
 read -n1 -r -p "Press any key to continue..."
-nano www/pianobar-eventcmd/update.py
+clear
+echo "Start pianobar in another terminal."
+echo "I'll wait."
+read -n1 -r -p "Press any key to continue..."
+echo 
+echo "We're going to start the Pidora webserver now. You can access it by going to http://127.0.0.1:8080 in a browser."
+echo "You should have full control pianobar. Experiment with it to be sure."
+echo "You can stop the server at any time by pressing Ctrl+C."
+python pidora/hello.py
+clear
+echo "Configuring the startup script."
+echo "rm /home/user/pidoraLocation/curSong
+@midori -a \"http://localhost:8080/\" -e Fullscreen
+@python pidora/hello.py
+@pianobar" | sudo tee -a /etc/xdg/lxsession/LXDE/autostart
+echo
+echo "Done!"
+echo "Reboot your Pi and enjoy!"
