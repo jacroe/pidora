18cee9b7cfb0b5f6f9596a3aafea3262f20cc2ed
Jacob Roeland <jacob@jacroe.com>
2013-08-18 15:40:32 -0500

    Beta TV interface  - Only difference is that the controls are removed.

--- template.py
+++ template.py
@@ -5,6 +5,9 @@ current_dir = os.path.dirname(os.path.abspath(__file__)) + "/"
 def index(songData):
 	return open(current_dir + "static/html/index.html").read()
 
+def tv(songData):
+	return open(current_dir + "static/htmltv.html").read()
+
 def mobile(songData):
 	returnData = mobileHead()
 	try:
